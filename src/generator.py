a6b481e94d09337e958ffbee6bd0fe8d56af5a25
Wojciech Trojanowski <W.Trojan27@gmail.com>
2021-06-09 22:07:08 +0200

    Extract graph generator to another class; Implement scenarios

---
+++ src/generator.py
@@ -0,0 +1,30 @@
+from random import choice, randint
+from graph import Graph
+
+class Generator():
+
+    def __init__ (self, order, max_edges_per_vertex):
+        self.graph = None
+        self.n_scenarios = 0
+        self.edges_weights = []
+
+        vertices = [v for v in range(order)]
+        edges = []
+        for v in vertices:
+            for i in range(0, max_edges_per_vertex):
+                tmp_vertices = vertices.copy()
+                tmp_vertices.pop(v)
+                edges.append([v, choice(tmp_vertices), 0])
+                self.edges_weights.append(randint(1,10))
+        self.graph = Graph(vertices, edges)
+
+        self.n_scenarios = len(self.edges_weights)
+
+    def generate_scenario (self) -> list:
+        self.edges_weights.append(self.edges_weights.pop(0))
+        print(self.edges_weights)
+
+        for i, e in enumerate(self.graph.edges):
+            e.weight = self.edges_weights[i]
+
+        return self.graph
--- src/main.py
+++ src/main.py
@@ -1,26 +1,16 @@
-from random import choice, randint
 from graph import Graph
+from generator import Generator
 from kruskal import Kruskal
 from prim import Prim
 from boruvka import Boruvka
 
 if __name__ == '__main__':
     # Generate random graphs
-    def generate_graphs (n_graphs, order, max_edges_per_vertex) -> list:
-        graphs = []
-        for g in range(n_graphs):
-            vertices = [v for v in range(order)]
-            edges = []
-            for v in vertices:
-                for i in range(0, max_edges_per_vertex):
-                    tmp_vertices = vertices.copy()
-                    tmp_vertices.pop(v)
-                    edges.append([v, choice(tmp_vertices), randint(1,10)])
-            graphs.append(Graph(vertices, edges))
+    gen = Generator(10, 2)
 
-        return graphs
-
-    graphs = generate_graphs (1, 20, 3)
+    graphs = []
+    graphs.append(gen.generate_scenario())
+    graphs.append(gen.generate_scenario())
 
     for g in graphs:
         print(g.get_adjacency_list())
