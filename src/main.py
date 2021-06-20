dcd95d978c9f25b10272310f4e24a321bdc6f8df
Wojciech Trojanowski <W.Trojan27@gmail.com>
2021-06-20 13:27:30 +0200

    Desubscribe prim's algorithm (the same results as Kruskal), Fix scenarios generator logic

--- src/main.py
+++ src/main.py
@@ -3,21 +3,16 @@ from generator import Generator
 from spanning_tree import Spanning_tree
 from kruskal import Kruskal
 from prim import Prim
-from boruvka import Boruvka
 
 if __name__ == '__main__':
 
     # Generate graph
-    gen = Generator(4, 2)
-
-    # Generate scenarios
-    scenarios = []
-    for scenario in gen.edges_weights:
-        scenarios.append(gen.generate_scenario())
+    gen = Generator(5, 2)
 
     results = []
-    for i, s in enumerate(scenarios):
-        # print(s.get_adjacency_list())
+    for i, s in enumerate(range(gen.n_scenarios)):
+        s = gen.generate_scenario()
+        print(s.get_adjacency_list())
 
         st = []
         model = Spanning_tree(s)
@@ -29,19 +24,16 @@ if __name__ == '__main__':
         model = Kruskal()
         kruskal_value = model.exec(s)
         
-        prim_values = []
-        for v in s.vertices:
-            model = Prim()
-            prim_values.append(model.exec(s, v))
-            prim_value = sorted(prim_values)[0]
-
+        # prim_values = []
+        # for v in s.vertices:
+        #     model = Prim()
+        #     prim_values.append(model.exec(s, v))
+        #     prim_value = sorted(prim_values)[0]
 
         results.append({
             'st': st,
             'mst_kruskal': kruskal_value,
-            'mst_prim': prim_value
+            # 'mst_prim': prim_value
         })
 
     print(results)
-
-    # boruvka_mst = Boruvka.exec(graph1)
